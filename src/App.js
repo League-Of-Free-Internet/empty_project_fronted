2e35476ac822f3a11d9e4c8e349bcb335b4d2048
Eduard Gumen <hydrospirt@mail.ru>
2024-07-26 18:56:49 +0300

    feat: Update App.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import "./App.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <div>
       <Header />
+      <h1>Тест</h1>
+      <Footer />
     </div>
   );
 }
