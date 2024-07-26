94aa501b3af7e9beb7cda5d8188e83221a8c9ac7
Eduard Gumen <hydrospirt@mail.ru>
2024-07-26 18:57:08 +0300

    feat: Add Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 function App() {
